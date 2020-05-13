7383bb23d7bc0b7520f3c63ed12813216c5c9647
elicaste <e.castelnuovo95@gmail.com>
2020-05-13 17:56:20 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -23,4 +23,27 @@ pdf("faPAR.pdf")
 levelplot(faPAR10)
 dev.off()
 
+####################################################### day 2
+
+setwd("C:/lab/") 
+load("faPAR.RData")
+library(raster)
+library(rasterdiv)
+library (rasterVis)
+
+#the original faPAR from Copernicus is  2GB
+# let's see how much space is needed for an 8-bit set
+
+writeRaster( copNDVI, "copNDVI.tif")
+# 5.3 MB
+
+# to make the level plot of the faPAR
+levelplot(faPAR10) #faPAR = fraction of the solar radiation absorbed by live leaves 
+
+
+
+
+
+
+
 
