ba75fd2a0168e8d29b5bd809cbaf2c774bd3290c
elicaste <e.castelnuovo95@gmail.com>
2020-06-03 12:28:11 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -1,14 +1,15 @@
-# R_code_EBVs.r
+### R_code_EBVs.r
 # measure the standar deviation from a satellite imagine 
 
 setwd("C:/lab/")
 
 library(raster)
-library/Rstoolbox) #for PCA
+library(Rstoolbox) #for PCA
 
-snt <- brick("snt_r10.tif")
+snt <- brick("snt_r10.tif") #brick function is used to create a multi-layer file
 plot(snt)
 
+#remember that:
 #B1 blue
 #B2 green
 #B3 red
@@ -18,24 +19,25 @@ plot(snt)
 plotRGB(snt, 3,2,1, stretch="lin")
 plotRGB(snt, 4,3,2, stretch="lin")
 
-pairs(snt)
+pairs(snt) #Scatterplot matrices
 
 ### PCA analysis
-
-sntpca <- rasterPCA(snt)
+sntpca <- rasterPCA(snt) # rasterPCA calculates R-mode PCA 
 sntpca
 summary(sntpca$model)
-#70% of information
-plot(sntpca$map)
-plotRGB(sntpca$map, 1,2,3, stretch="lin")
 
-# calculate the standard deviation 
+#70% of information
+plot(sntpca$map) 
+# make a Red-Green-Blue plot based on three layers
+plotRGB(sntpca$map, 1,2,3, stretch="lin") 
+
+#calculate the standard deviation 
 #set the moving window
-window <- matrix(1, nrow=5, ncol=5)
+window <- matrix(1, nrow=5, ncol=5) #matrix function is used to create a matrix defined by the arguments nrow and ncol
 window
 
-#focal function                   #first principal component
-sd_snt <- focal(sntpca$map$PC1, w=window, fun=sd)
+#focal function calculates values for the neighborhood of focal cells component
+sd_snt <- focal(sntpca$map$PC1, w=window, fun=sd) #fun=the function to be applied
 cl <- colorRampPalette(c("dark blue", "green", "orange", "red"))(100)
 plot(sd_snt, col=cl)
 
@@ -44,21 +46,21 @@ plotRGB(snt,4,3,2, stretch="lin", main="original image")
 plot(sd_snt, col=cl, main="diversity")
 
 ############### day 2
-# Focal on Cladonia stellaris 
+### Focal on Cladonia stellaris 
 
 library(raster)
 library(RStoolbox)
 
 setwd("C:/lab/")
 
-#to import the file we can cuse the raster function (that imports a single layer) or brick function (that imports different layers) 
+#to import the file we can use the raster function (that imports a single layer) or brick function (that imports different layers) 
 clad <- brick("cladonia_stellaris_calaita.JPG")  
 
-#matrix of 3 by 3 pixels, numer 1 doesn't impact the calculation 
+#matrix of 3 by 3 pixels, number 1 doesn't impact the calculation 
 window <- matrix(1, nrow = 3, ncol = 3)
 window
 
-# PCA analyssis for Cladonia 
+# PCA analysis for Cladonia 
 cladpca <- rasterPCA(clad)
 cladpca #to see all the data that are output of this function 
 summary(cladpca$model) #we can see that the proportion of variance is 0.98 that means the 98% of the first component 
@@ -71,7 +73,7 @@ plotRGB(cladpca$map, 1,2,3, stretch="lin")
 window <- matrix(1, nrow=5, ncol=5)
 window
 
-# focal function is going to calculate values for neighborhood of focal cells
+#focal function is going to calculate values for neighborhood of focal cells
 sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd) 
 #aggregate function
 PC1_agg <- aggregate(cladpca$map$PC1, fact=10)
