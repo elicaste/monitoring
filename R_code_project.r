b657bf998bc80912309bccdf8b4500bde790b5c7
Elisa Castelnuovo <e.castelnuovo95@gmail.com>
2020-07-17 22:37:02 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -15,4 +15,15 @@ library(rgdal) # package for geospatial analysis
 library(RStoolbox)
 library(gdalUtils) #to convert hdf file 
 
+# data of LAND TEMPERATURE 
+# to quikly import all the data together use the the lapply function 
+setwd("C:/lab/temp/")
+watertemp_list<- list.files(pattern = "AQUA_MODIS.2009")
+watertemp_list #see the files name 
+import<- lapply(watertemp_list, raster) 
+watertemp.multitemp<- stack(import)
+cl <- colorRampPalette(c("blue","green","yellow", "orange","red"))(10000)
+plot(watertemp.multitemp, col=cl)
+
+
 
