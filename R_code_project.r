96e72368d39013676b1cff29589d7fdec95432c4
Elisa Castelnuovo <e.castelnuovo95@gmail.com>
2020-09-03 16:45:55 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -1,5 +1,5 @@
 #R_code_project.r
-setwd("C:/lab/incendi") #set the working director
+setwd("C:/lab/California") #set the working director
 
 # install all packages needed 
 install.packages ("raster") #package for the analysis and geographyc data manipulation 
@@ -12,7 +12,6 @@ install.packages("RStoolbox")
 install.packages("gdalUtils")
 install.packages("plot.matrix")
 install.packages("RasterLayer")
-install.packages ("devtools") # Strumenti di pacchetto per rendere più facile lo sviluppo di pacchetti R
 
 # load the packages in the library 
 library(raster)
@@ -24,190 +23,17 @@ library(RStoolbox)
 library(gdalUtils) #to convert hdf file 
 library(plot.matrix)
 library(RasterLayer)
-library(devtools)
-
-install.packages ("maptools") # Pacchetto di strumenti per leggere e gestire oggetti spaziali
-install.packages ("rgeos") # Interfaccia per Geometry Engine - Open Source (GEOS)
-install.packages ("RCurl") # Fornisce funzioni per consentire di comporre richieste HTTP generali e di recuperare gli URI
-
-
-prefire2020<- "LC80010852017008LGN00" 	
-postfire2020<- "LC80010852017056LGN00" 
-prefire2013<- "LC80010852017008LGN00" 	
-postfire2013<- "LC80010852017056LGN00"
-
-lspre2013<- ReadLandsat8(prefire2013) #read images 2013 pre-fire
-lspost2013<- ReadLandsat8(postfire2013) #read  images 2013 post-fire
-lspre2020<- ReadLandsat8(prefire2020) #read  images 2020 pre-fire
-lspost2020<- ReadLandsat8(postfire2020) #read  images 2020 post-fire
-
-
-______________________________________________________________________________________________________________
-
-install.packages("rasterdiv")
-install.packages("rasterVis")
-install.packages("ncdf4")
-install.packages("rgdal")
-install.packages("RStoolbox")
-install.packages("gdalUtils")
-install.packages("RasterLayer")
-install.packages("plot.matrix")
-library(raster) ## package for raster manipulation: imports a single layer, yet satellite images are made of more than one layer
-library(rasterVis)
-library(ncdf4) # package for netcdf manipulation
-library(rgdal) # package for geospatial analysis
-library(RStoolbox)
-library(gdalUtils) #to convert hdf file 
-library(plot.matrix)
-
-# 1. temperature --> see if change through the time 
-#                    scaricare ogni mese per 7 anni 
-#                    media annuale -> plot per vedere l'andamento dal 2014 al 2020
-
-
-# 2. vegetation cover --> Fraction of green Vegetation Cover
-#                         The Fraction of Vegetation Cover (FCover) corresponds to the fraction of ground covered by green vegetation. 
-#                         Practically, it quantifies the spatial extent of the vegetation. 
-#                         Because it is independent from the illumination direction and it is sensitive to the vegetation amount, 
-#                         FCover is a very good candidate for the replacement of classical vegetation indices for the monitoring of ecosystems.
-#                    scaricare ogni mese per 4 anni   
-#                    media annuale -> plot per vedere l'andamento dal 2017 al 2020
-# 3. plot RGB e NIR 
-# 4. vedo effetti di questi cambiamenti
-
-
-######################################### data of LAND TEMPERATURE : import file efrom earthdata.nasa.gov     
-
-setwd("C:/lab/Land_temperature")
-landtemp_list<- list.files(pattern = "LST") #select all the file whose name contains "FCOVER"
-landtemp_list #see the files name 
-import_landtemp<- lapply(landtemp_list, raster) 
-landtemp.multitemp<- stack(import_landtemp)
-
-#zoom on the zone near San Francisco
-extCal <- c(-124,-121, 37,39)
-cl_temp <- colorRampPalette(c("blue", "lightblue", "yellow", "orange", "red"))(100)
-zoom(landtemp.multitemp$Fraction.of.Valid.Observations.1 , ext=extCal, col=cl_temp)
-
-#cut the previous image 
-land_temp_201706<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.1, extCal)
-land_temp_201707<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.2, extCal)
-land_temp_201708<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.3, extCal)
-land_temp_201709<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.4, extCal)
-land_temp_201710<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.5, extCal)
-land_temp_201711<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.6, extCal)
-land_temp_201712<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.7, extCal)
-land_temp_201801<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.8, extCal)
-land_temp_201802<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.9, extCal)
-land_temp_201803<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.10, extCal)
-land_temp_201804<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.11, extCal)
-land_temp_201805<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.12, extCal)
-land_temp_201806<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.13, extCal)
-land_temp_201807<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.14, extCal)
-land_temp_201808<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.14, extCal)
-land_temp_201809<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.16, extCal)
-land_temp_201810<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.17, extCal)
-land_temp_201811<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.18, extCal)
-land_temp_201812<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.19, extCal)
-land_temp_201901<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.20, extCal)
-land_temp_201902<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.21, extCal)
-land_temp_201903<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.22, extCal)
-land_temp_201904<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.23, extCal)
-land_temp_201905<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.24, extCal)
-land_temp_201906<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.25, extCal)
-land_temp_201907<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.26, extCal)
-land_temp_201908<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.27, extCal)
-land_temp_201909<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.28, extCal)
-land_temp_201910<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.29, extCal)
-land_temp_201911<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.30, extCal)
-land_temp_201912<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.31, extCal)
-land_temp_202001<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.32, extCal)
-land_temp_202002<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.33, extCal)
-land_temp_202003<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.34, extCal)
-land_temp_202004<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.35, extCal)
-land_temp_202005<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.36, extCal)
-land_temp_202006<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.37, extCal)
-
-#convert single raster layer to matrix
-land_temp_201706_matrix <- raster::as.matrix(land_temp_201706) 
-
-plot.matrix(land_temp_201706_matrix)
-
-saveDataset(land_temp_201706_matrix)
-plot(land_temp_201706_matrix)
-image(as.matrix(land_temp_201706))
-land_temp_201706_matrix <- raster2matrix(land_temp_201706)
-# see the difference between 2020 and 2000
-difftemp <- 
-
-  
-  
-
-######################################### data of VEGETATION COVER: import file from Copernicus 
-# to quikly import all the data together use the the lapply function 
-setwd("C:/lab/Vegetation_cover_Copernicus/")
-vegcover_list<- list.files(pattern = "FCOVER") #select all the file whose name contains "FCOVER"
-vegcover_list #see the files name 
-import<- lapply(vegcover_list, raster) 
-vegcover.multitemp<- stack(import)
-
-#zoom on California
-extCal <- c(-125,-105, 30,50)
-zoom(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.1 , ext=extCal)
-#cut the previous image 
-veg_cover_201706<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.1, extCal)
-veg_cover_201707<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.2, extCal)
-veg_cover_201708<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.3, extCal)
-veg_cover_201709<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.4, extCal)
-veg_cover_201710<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.5, extCal)
-veg_cover_201711<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.6, extCal)
-veg_cover_201712<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.7, extCal)
-veg_cover_201801<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.8, extCal)
-veg_cover_201802<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.9, extCal)
-veg_cover_201803<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.10, extCal)
-veg_cover_201804<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.11, extCal)
-veg_cover_201805<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.12, extCal)
-veg_cover_201806<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.13, extCal)
-veg_cover_201807<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.14, extCal)
-veg_cover_201808<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.14, extCal)
-veg_cover_201809<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.16, extCal)
-veg_cover_201810<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.17, extCal)
-veg_cover_201811<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.18, extCal)
-veg_cover_201812<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.19, extCal)
-veg_cover_201901<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.20, extCal)
-veg_cover_201902<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.21, extCal)
-veg_cover_201903<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.22, extCal)
-veg_cover_201904<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.23, extCal)
-veg_cover_201905<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.24, extCal)
-veg_cover_201906<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.25, extCal)
-veg_cover_201907<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.26, extCal)
-veg_cover_201908<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.27, extCal)
-veg_cover_201909<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.28, extCal)
-veg_cover_201910<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.29, extCal)
-veg_cover_201911<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.30, extCal)
-veg_cover_201912<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.31, extCal)
-veg_cover_202001<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.32, extCal)
-veg_cover_202002<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.33, extCal)
-veg_cover_202003<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.34, extCal)
-veg_cover_202004<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.35, extCal)
-veg_cover_202005<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.36, extCal)
-veg_cover_202006<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.37, extCal)
-
-#see the difference between june 2017 and june 2020
-diff_veg_cover_2017_2020<- veg_cover_202006 - veg_cover_201706
-cldiff<- colorRampPalette(c("blue", "black", "yellow"))(100)
-plot(diff_veg_cover_2017_2020, col=cldiff)
 
 ######################################### RGB and NDVI analysis 
 
 #import 2018 images
-setwd("C:/lab/EO_images2018")
-rlist_2018<- list.files(pattern="2018")
-rlist_2018
-import_images_2018 <- lapply(rlist_2018, raster)
-images_2018 <- stack(import_images_2018)
+setwd("C:/lab/California/2018_June")
+rlist_2018_June<- list.files(pattern="2018")
+rlist_2018_June 
+import_images_2018_June <- lapply(rlist_2018, raster)
+images_2018_June <- stack(import_images_2018_June)
 cl <- colorRampPalette(c('red','orange','yellow'))(100) 
-plot(images_2018, col=cl) # to see all the bands 
+plot(images_2018:June, col=cl) # to see all the bands 
 
 #import 2020 images
 setwd("C:/lab/EO_images2020")
@@ -319,4 +145,75 @@ plot(ndvi2020, col=cl)
 diff_dvi <- dvi2020 - dvi2017
 cldiff<- colorRampPalette(c("peachpuff", "mistyrose1", "darkorchid4"))(100)
 plot(diff_dvi, col=cldiff)
+
+
+
+
+# 1. temperature --> see if change through the time 
+#                    scaricare ogni mese per 7 anni 
+#                    media annuale -> plot per vedere l'andamento dal 2014 al 2020
+
+
+# 2. vegetation cover --> Fraction of green Vegetation Cover
+#                         The Fraction of Vegetation Cover (FCover) corresponds to the fraction of ground covered by green vegetation. 
+#                         Practically, it quantifies the spatial extent of the vegetation. 
+#                         Because it is independent from the illumination direction and it is sensitive to the vegetation amount, 
+#                         FCover is a very good candidate for the replacement of classical vegetation indices for the monitoring of ecosystems.
+#                    scaricare ogni mese per 4 anni   
+#                    media annuale -> plot per vedere l'andamento dal 2017 al 2020
+# 3. plot RGB e NIR 
+# 4. vedo effetti di questi cambiamenti
+
+
+######################################### data of LAND TEMPERATURE : import file efrom earthdata.nasa.gov     
+
+setwd("C:/lab/Land_temperature")
+landtemp_list<- list.files(pattern = "LST") #select all the file whose name contains "FCOVER"
+landtemp_list #see the files name 
+import_landtemp<- lapply(landtemp_list, raster) 
+landtemp.multitemp<- stack(import_landtemp)
+
+#zoom on the zone near San Francisco
+extCal <- c(-124,-121, 37,39)
+cl_temp <- colorRampPalette(c("blue", "lightblue", "yellow", "orange", "red"))(100)
+zoom(landtemp.multitemp$Fraction.of.Valid.Observations.1 , ext=extCal, col=cl_temp)
+
+#cut the previous image 
+land_temp_201706<-crop(landtemp.multitemp$Fraction.of.Valid.Observations.1, extCal)
+
+#convert single raster layer to matrix
+land_temp_201706_matrix <- raster::as.matrix(land_temp_201706) 
+
+plot.matrix(land_temp_201706_matrix)
+
+saveDataset(land_temp_201706_matrix)
+plot(land_temp_201706_matrix)
+image(as.matrix(land_temp_201706))
+land_temp_201706_matrix <- raster2matrix(land_temp_201706)
+# see the difference between 2020 and 2000
+difftemp <- 
+
+  
+  
+
+######################################### data of VEGETATION COVER: import file from Copernicus 
+# to quikly import all the data together use the the lapply function 
+setwd("C:/lab/Vegetation_cover_Copernicus/")
+vegcover_list<- list.files(pattern = "FCOVER") #select all the file whose name contains "FCOVER"
+vegcover_list #see the files name 
+import<- lapply(vegcover_list, raster) 
+vegcover.multitemp<- stack(import)
+
+#zoom on California
+extCal <- c(-125,-105, 30,50)
+zoom(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.1 , ext=extCal)
+#cut the previous image 
+veg_cover_201706<-crop(vegcover.multitemp$Fraction.of.green.Vegetation.Cover.1km.1, extCal)
+
+#see the difference between june 2017 and june 2020
+diff_veg_cover_2017_2020<- veg_cover_202006 - veg_cover_201706
+cldiff<- colorRampPalette(c("blue", "black", "yellow"))(100)
+plot(diff_veg_cover_2017_2020, col=cldiff)
+
+
 
